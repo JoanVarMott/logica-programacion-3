dd0d390b9e8b49351689c9e9da6e5aec4491f0db
JoanVarMott <soers.joan@gmail.com>
2024-02-09 10:07:11 -0500

    logica de programacion 3- factorial de un numero

---
+++ script.js
@@ -0,0 +1,26 @@
+function factorial() {
+    const inputNumber = parseInt(document.getElementById('inputNumber').value);
+
+    if (isNaN(inputNumber) || inputNumber < 0) {
+        document.getElementById('resultado').textContent = 
+        'Error: ingresa un número positivo válido.';
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "ingresa un numero positivo!",
+          });
+    } else {
+        let factorial = 1;
+        for (let i = 1; i <= inputNumber; i++) {
+            factorial *= i;
+        }
+        document.getElementById('resultado').textContent = 
+        `El factorial de ${inputNumber} es ${factorial}.`;
+        Swal.fire({
+            // position: "top-end",
+            icon: "success",
+            title: `El factorial de ${inputNumber} es ${factorial}.`,
+            showConfirmButton: false,
+          });
+    }
+}
